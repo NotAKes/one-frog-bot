424332b0fa6e15c46b2fcc2db0a45843c0dd744b
NotAkes <anton_fcsm920@mail.ru>
2024-06-24 23:54:55 +0300

    handlers file unknown commands support

---
+++ func_handlers.py
@@ -0,0 +1,191 @@
+import asyncio
+from aiogram.filters.command import Command
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram import F, types
+from aiogram import Router
+import datetime
+from parseit import parser
+from config import cities
+
+router = Router()
+
+
+@router.message(F.text == "О боте")
+async def github_func(message: types.Message):
+    url_buttons = InlineKeyboardBuilder()
+    url_buttons.row(
+        types.InlineKeyboardButton(text='Сообщить об ошибке', url='https://github.com/NotAKes/aowbee_bot/issues'))
+    url_buttons.row(types.InlineKeyboardButton(text='Github проекта', url='https://github.com/NotAKes/AOWBee_bot'))
+    url_buttons.row(types.InlineKeyboardButton(text='Написать разработчику', url='t.me/notakees'))
+    await message.answer('Выберите ссылку🔗', reply_markup=url_buttons.as_markup(), )
+
+
+@router.message(F.text == "Москва")
+async def place_msk_func(message: types.Message):
+    global place
+    place = ''
+    place = cities["MSK"]
+    await choosetime_func(message)
+
+
+@router.message(F.text == "Орехово-Зуево")
+async def place_oz_func(message: types.Message):
+    global place
+    place = ''
+    place = cities["OZ"]
+    await choosetime_func(message)
+
+
+@router.message(F.text == "Астана")
+async def place_ast_func(message: types.Message):
+    global place
+    place = ''
+    place = cities["AST"]
+    await choosetime_func(message)
+
+
+@router.message(F.text == "С.Петербург")
+async def place_spb_func(message: types.Message):
+    global place
+    place = ''
+    place = cities["SPB"]
+    await choosetime_func(message)
+
+
+@router.message(F.text == "Минск")
+async def place_minsk_func(message: types.Message):
+    global place
+    place = ''
+    place = cities["MINSK"]
+    await choosetime_func(message)
+
+
+@router.message(F.text == "Сегодня🕓")
+async def today(message: types.Message):
+    global date
+    date = 1
+    if place:
+        await message.reply(
+            f" <b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now()).date(), "%d.%m.%Y")}\n</b>"
+            f"\n"
+            f"{parser(date, list_=place)}", parse_mode="html")
+    else:
+        await message.reply("Место не выбрано")
+        await choose_place(message)
+
+
+@router.message(F.text == "Завтра🕓")
+async def yesterday(message: types.Message):
+    global date
+    date = 2
+    if place:
+        await message.reply(
+            f"<b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now() + datetime.timedelta(days=1)).date(), "%d.%m.%Y")}</b>\n"
+            f"\n"
+            f"{parser(date, list_=place)}", parse_mode="html")
+    else:
+        await message.reply("Место не выбрано")
+        await choose_place(message)
+
+
+@router.message(F.text == "3 дня🗓")
+async def threedays(message: types.Message):
+    if place:
+        for date in range(2, 5):
+            await message.answer(
+                f" <b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now() + datetime.timedelta(days=date - 1)).date(), "%d.%m.%Y")}</b> \n"
+                f"\n{parser(date, list_=place)}", parse_mode="html")
+    else:
+        await message.reply("Место не выбрано")
+        await choose_place(message)
+
+
+@router.message(F.text == "Неделя🗓")
+async def week(message: types.Message):
+    if place:
+        for date in range(2, 9):
+            await message.answer(
+                f" <b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now() + datetime.timedelta(days=date - 1)).date(), "%d.%m.%Y")}</b> \n"
+                f"\n"
+                f"{parser(date, list_=place)}", parse_mode="html")
+    else:
+        await message.reply("Место не выбрано")
+        await choose_place(message)
+
+
+# Команда /menu
+@router.message(Command("menu"))
+async def call_menu(message: types.Message):
+    await menu(message)
+
+
+# Команда /place
+@router.message(Command("place"))
+async def call_place(message: types.Message):
+    await choose_place(message)
+
+
+# Команда /start
+@router.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Привет!👋 Я бот, определяющий погоду:")
+    await menu(message)
+
+
+@router.message(F.text == "Прогноз погоды")
+async def choose_place(message: types.Message):
+    inp = [
+        [
+            types.KeyboardButton(text="Москва"),
+            types.KeyboardButton(text="С.Петербург"),
+            types.KeyboardButton(text="Орехово-Зуево")
+        ],
+
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=inp,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите регион из списка доступных"
+    )
+    await message.answer(
+        f"Выберите регион из списка доступных:\n\n 🇷🇺 Москва, С.Петербург, Орехово-Зуево\n 🇧🇾 Минск \n 🇰🇿 Астана \n {"-" * 40} \nДля выбора напишите название города или нажмите на кнопку",
+        reply_markup=keyboard)
+
+
+@router.message()
+async def unknown_message(message: types.Message):
+    await message.answer(
+        "Похоже, я не знаю эту команду. Используйте /menu. Если вы думаете что произошла ошибка, то напишите @notakees")
+
+
+async def choosetime_func(message: types.Message):
+    inp = [
+        [
+            types.KeyboardButton(text="Сегодня🕓"),
+            types.KeyboardButton(text="Завтра🕓"),
+            types.KeyboardButton(text="3 дня🗓"),
+            types.KeyboardButton(text="Неделя🗓")
+        ],
+
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=inp,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите время предсказания..."
+    )
+    await message.answer("Выберите время🕓", reply_markup=keyboard)
+
+
+async def menu(message: types.Message):
+    inp = [
+        [
+            types.KeyboardButton(text="О боте"),
+            types.KeyboardButton(text="Прогноз погоды")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=inp,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите функцию"
+    )
+    await message.answer("Выберите функцию⚙️", reply_markup=keyboard)
--- main.py
+++ main.py
@@ -1,207 +1,23 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
-from aiogram import F
+from aiogram import Bot, Dispatcher
+import func_handlers
 import asyncio
 import logging
-import datetime
 import os
 from dotenv import load_dotenv, find_dotenv
-from parseit import parser
-from config import cities
-
-# логгирование
-logging.basicConfig(level=logging.INFO, filename="logs.log", filemode="w",
-                    format="[%(asctime)s] %(levelname)s %(message)s")
-
-# переменные места и времени
-date = ''
-
-MOSCOW = cities[0]
-MINSK = cities[1]
-SPB = cities[2]
-AST = cities[3]
-OZ = cities[4]
-
-place = ''
-
-# начало логики бота
-load_dotenv(find_dotenv())
-bot = Bot(token=os.getenv('TOKEN'))
-dp = Dispatcher()
-
-
-@dp.message(F.text == "Github проекта")
-async def github_func(message: types.Message):
-    await message.answer("https://github.com/NotAKes/AOWBee_bot")
-
-
-@dp.message(F.text == "Москва")
-async def timeMSK_func(message: types.Message):
-    global place
-    place = ''
-    place = MOSCOW
-    await choosetime_func(message)
-
-
-@dp.message(F.text == "Орехово-Зуево")
-async def timeOZ_func(message: types.Message):
-    global place
-    place = ''
-    place = OZ
-    await choosetime_func(message)
-
-
-@dp.message(F.text == "Астана")
-async def timeAST_func(message: types.Message):
-    global place
-    place = ''
-    place = AST
-    await choosetime_func(message)
-
-
-@dp.message(F.text == "С.Петербург")
-async def timeSPB_func(message: types.Message):
-    global place
-    place = ''
-    place = SPB
-    await choosetime_func(message)
-
-
-@dp.message(F.text == "Минск")
-async def timeMIN_func(message: types.Message):
-    global place
-    place = ''
-    place = MINSK
-    await choosetime_func(message)
-
-
-@dp.message(F.text == "Сегодня🕓")
-async def today(message: types.Message):
-    global date
-    date = 1
-    if place:
-        await message.reply(
-            f" <b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now()).date(), "%d.%m.%Y")}\n</b>"
-            f"\n"
-            f"{parser(date, list_=place)}", parse_mode="html")
-    else:
-        await message.reply("Место не выбрано")
-        await choose_place(message)
-
-
-@dp.message(F.text == "Завтра🕓")
-async def yesterday(message: types.Message):
-    global date
-    date = 2
-    if place:
-        await message.reply(
-            f"<b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now() + datetime.timedelta(days=1)).date(), "%d.%m.%Y")}</b>\n"
-            f"\n"
-            f"{parser(date, list_=place)}", parse_mode="html")
-    else:
-        await message.reply("Место не выбрано")
-        await choose_place(message)
-
-
-@dp.message(F.text == "3 дня🗓")
-async def threedays(message: types.Message):
-    if place:
-        for date in range(2, 5):
-            await message.answer(
-                f" <b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now() + datetime.timedelta(days=date - 1)).date(), "%d.%m.%Y")}</b> \n"
-                f"\n{parser(date, list_=place)}", parse_mode="html")
-    else:
-        await message.reply("Место не выбрано")
-        await choose_place(message)
-
-
-@dp.message(F.text == "Неделя🗓")
-async def week(message: types.Message):
-    if place:
-        for date in range(2, 9):
-            await message.answer(
-                f" <b>Прогноз погоды на {datetime.datetime.strftime((datetime.datetime.now() + datetime.timedelta(days=date - 1)).date(), "%d.%m.%Y")}</b> \n"
-                f"\n"
-                f"{parser(date, list_=place)}", parse_mode="html")
-    else:
-        await message.reply("Место не выбрано")
-        await choose_place(message)
-
-
-# Команда /menu
-@dp.message(Command("menu"))
-async def call_menu(message: types.Message):
-    await menu(message)
-
-
-# Команда /place
-@dp.message(Command("place"))
-async def call_place(message: types.Message):
-    await choose_place(message)
-
-
-# Команда /start
-@dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Привет!👋 Я бот, определяющий погоду:")
-    await menu(message)
-
-
-async def menu(message: types.Message):
-    inp = [
-        [
-            types.KeyboardButton(text="Github проекта"),
-            types.KeyboardButton(text="Прогноз погоды")
-        ],
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=inp,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите функцию"
-    )
-    await message.answer("Выберите функцию⚙️", reply_markup=keyboard)
-
 
 async def main():
+    # логгирование
+    logging.basicConfig(level=logging.INFO, filename="logs.log", filemode="w",
+                        format="[%(asctime)s] %(levelname)s %(message)s")
+
+    load_dotenv(find_dotenv())
+    bot = Bot(token=os.getenv('TOKEN'))
+    dp = Dispatcher()
+
+    dp.include_router(func_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-
-
-@dp.message(F.text == "Прогноз погоды")
-async def choose_place(message: types.Message):
-    inp = [
-        [
-            types.KeyboardButton(text="Москва"),
-            types.KeyboardButton(text="С.Петербург"),
-            types.KeyboardButton(text="Орехово-Зуево")
-        ],
-
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=inp,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите регион из списка доступных"
-    )
-    await message.answer(
-        f"Выберите регион из списка доступных:\n\n 🇷🇺 Москва, С.Петербург, Орехово-Зуево\n 🇧🇾 Минск \n 🇰🇿 Астана \n {"-" * 40} \nДля выбора напишите название города или нажмите на кнопку",
-        reply_markup=keyboard)
-
-
-async def choosetime_func(message: types.Message):
-    inp = [
-        [
-            types.KeyboardButton(text="Сегодня🕓"),
-            types.KeyboardButton(text="Завтра🕓"),
-            types.KeyboardButton(text="3 дня🗓"),
-            types.KeyboardButton(text="Неделя🗓")
-        ],
-
-    ]
-    keyboard = types.ReplyKeyboardMarkup(
-        keyboard=inp,
-        resize_keyboard=True,
-        input_field_placeholder="Выберите время предсказания..."
-    )
-    await message.answer("Выберите время🕓", reply_markup=keyboard)
 
 
 if __name__ == "__main__":
